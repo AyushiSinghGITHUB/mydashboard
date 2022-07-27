7436e5da43d3b3801eece97a0f63973008801058
Ayushi <93368157+AyushiSinghGITHUB@users.noreply.github.com>
2022-07-27 12:32:35 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json
 app = Flask(__name__)
 
 # two decorators, same function
@@ -35,5 +35,74 @@ def myth6():
 def myth7():
     return render_template('chart7.html')
 
+@app.route("/getdata",methods=["POST","GET"])
+def getdata():
+    data=[{
+            "x": 0,
+            "y": 0,
+            "width": 10,
+            "height": 1,
+            "div_id": "b1",
+            "chart" : '/cards.html'
+        },
+        {
+            "x": 0,
+            "y": 7,
+            "width": 6,
+            "height": 3,
+            "div_id": "3",
+            "chart" : '/chart7.html'
+        },
+        {
+            "x": 6,
+            "y": 7,
+            "width": 6,
+            "height": 3,
+            "div_id": "a2sssss",
+            "chart" : '/chart6.html'
+        },
+        {
+            "x": 6,
+            "y": 4,
+            "width": 6,
+            "height": 3,
+            "div_id": "sdfsdfa2",
+            "chart" : '/chart5.html'
+        },
+        {
+            "x": 10,
+            "y": 0,
+            "width": 2,
+            "height": 2,
+            "div_id": "2",
+            "chart" : '/base.html'
+        },
+        {
+            "x": 0,
+            "y": 1,
+            "width": 5,
+            "height": 3,
+            "div_id": "a2",
+            "chart" : '/chart4.html'
+        },
+        {
+            "x": 5,
+            "y": 1,
+            "width": 5,
+            "height": 3,
+            "div_id": "a1",
+            "chart" : '/myth.html'
+        },
+        {
+            "x": 0,
+            "y": 4,
+            "width": 6,
+            "height": 3,
+            "div_id": "1",
+            "chart" :'/symbol.html'
+        }]
+    return json.dumps(data)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
